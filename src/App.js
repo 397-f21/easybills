b36a51f149cfe94c61e8f2101371463705860f87
lyfwooow <43867436+lyfwooow@users.noreply.github.com>
2021-11-12 14:27:11 -0600

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const total_amount = 100;
+const people_amount = 3;
+const dish1 = 25;
+const dish2 = 50;
+const dish3 = 25;
+
+const tip = total_amount * 0.20;
+const user1 = dish1 / total_amount * tip + dish1;
+// your share = (total+tip) / menu total * what you ordered
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="easyBills">
+
     </div>
   );
 }
