76813683e467cba1bf8ec7988852079a79c9c17b
Brian Tang <btang.tsp@gmail.com>
2021-11-17 17:09:47 -0600

    added tip input & calc; improved ui

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
   const [totalBill, setTotalBill] = useState(0);
   const [numPeople, setNumPeople] = useState(1);
   const [evenResult, setEvenResult] = useState(0);
+  const [tip, setTip] = useState(0);
 
   const changeTotalBill = (e) => {
     setTotalBill(e.target.value);
@@ -29,28 +30,47 @@ function App() {
   }
 
   const calculateTotal = () => {
-    result = (totalBill / numPeople).toString();
+    result = (totalBill / numPeople * (1 + tip)).toString();
     setEvenResult(result);
+  }
+
+  const handleTipChange = (e) => {
+    setTip(e.target.value);
   }
 
   return (
     <div className="easyBills">
-      
       <div className="container">
-      <h1>Easybills</h1>
-        <div class="form-group">
-          <label for="billAmt">Total Bill</label>
+        <div className="center">
+          <h1>Easybills</h1>
+        </div>
+        <div className="form-group">
+          <label for="billAmt">Total Bill (without Tip)</label>
           <input type="text" class="form-control" id="billAmt" placeholder="Enter total bill" onChange={(e) => changeTotalBill(e)}></input>
           <small id="emailHelp" class="form-text text-muted">For splitting evenly, enter the total bill including tip.</small>
         </div>
-        <div>
+        <div className="form-group">
+          <label>Tip Amount</label>
+          <select className="form-select" onChange={e => handleTipChange(e)}>
+            <option selected>Choose Tip Percentage (%)</option>
+            <option value="0">0%</option>
+            <option value=".15">15%</option>
+            <option value=".18">18%</option>
+            <option value=".20">20%</option>
+          </select>
+        </div>
+        <div className="form-group">
           <label for="numPeople">Number of People</label>
           <input type="text" class="form-control" id="numPeople" placeholder="Enter total number of people" onChange={(e) => changeNumPeople(e)}></input>
         </div>
-        <button className="btn btn-primary" onClick={calculateTotal}>Calculate</button>
-        <div>{evenResult}</div>
-  </div>
+        <div className="center">
+          <button className="btn btn-primary" onClick={calculateTotal}>Calculate</button>
+        </div>
+        <div className="form-group center">
+          <h2>Each Person Should Pay: {`$${evenResult}`}</h2>
+        </div>
       </div>
+    </div>
   );
 }
 
